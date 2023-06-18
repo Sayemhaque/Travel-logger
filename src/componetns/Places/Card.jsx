97093bbbbc575b8a28e36974e39df312a621f862
sayemhaque <mdsayemofficial1000@gmail.com>
2023-06-19 02:21:59 +0600

    places and sidebar ui modified

--- src/componetns/Places/Card.jsx
+++ src/componetns/Places/Card.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import Rating from "react-rating";
 import location from "../../assets/placeholder.png"
-import { FaStar, } from "react-icons/fa";
+import { FaArrowAltCircleRight, FaStar, } from "react-icons/fa";
 
 const Card = ({ place }) => {
     return (
-        <div className="rounded-lg flex flex-col md:flex-row  gap-5 shadow-lg">
+        <div className="rounded-lg  bg-white flex flex-col md:flex-row  gap-5 shadow-lg">
             {/* image */}
             <div>
                 <img className="w-full object-cover md:w-[200px] h-full" src={place.image} alt="" />
@@ -14,7 +14,9 @@ const Card = ({ place }) => {
             <div className="px-4 py-2 md:w-8/12">
                 <h2 className="text-md md:text-lg font-bold">{place.title}</h2>
                 <p className="mt-3 w-/12">{place.details}</p>
-                <Rating
+                {/* Rating */}
+              <div className="flex items-center gap-2">
+              <Rating
                     className="mt-2"
                     readonly
                     placeholderRating={place.rating}
@@ -22,9 +24,16 @@ const Card = ({ place }) => {
                     placeholderSymbol={<FaStar className="text-green-400" />}
                     fullSymbol={<FaStar className="text-green-500" />}
                 />
+                <p className="mt-1">{place.rating}</p>
+              </div>
+                <div className="flex justify-between items-center">
+                <div >
                 <h3 className="flex pb-2 items-center gap-1 mt-2 text-sm font-bold">
                     <img className="w-5 h-5" src={location} alt="" />
                     {place.location}</h3>
+                </div>
+                <FaArrowAltCircleRight title="see details" className="text-lg text-green-500 cursor-pointer"/>
+                </div>
             </div>
         </div>
     );
--- src/componetns/Sidebar/Sidebar.jsx
+++ src/componetns/Sidebar/Sidebar.jsx
@@ -1,27 +1,42 @@
 import { useEffect, useState } from "react";
 
 const Sidebar = () => {
-    const [category,setCategory] = useState([])
+    const [category, setCategory] = useState([])
     // get categories
     useEffect(() => {
         const getData = async () => {
-          const res = await fetch("places.json")
-          const data  = await res.json()
-          setCategory(data)
+            const res = await fetch("places.json")
+            const data = await res.json()
+            setCategory(data)
         }
         getData()
-    } ,[])
+    }, [])
     return (
-        <div className="hidden md:inline-flex shadow-lg border border-gray-300  sticky top-0 h-screen">
-           <ul className="flex flex-col gap-5 text-md font-bold pl-12 py-12">
-              {category.map(item => 
-             <div className="flex items-center gap-5" key={item.image}>
-              <input type="checkbox"  name="" id="" />
-              <li className="text-slate-800">{item.category}</li>
-             </div>
-              )}
-           </ul>
+        <div className="shadow-lg bg-white border border-gray-300 md:sticky top-0 md:h-[600px] p-5 ">
+            {/* category options */}
+            <p className="mb-1">Filter by Category</p>
+            <select className="select w-full max-w-md border border-green-500">
+                <option disabled selected>Category</option>
+                {category.map((item) => (
+                    <option value={item.category} key={item.image} className="text-slate-800">
+                        {item.category}
+                    </option>
+                ))}
+            </select>
+            {/* district options */}
+            <div className="form-control w-full max-w-md mt-6">
+                <p className="mb-1">Filter by District</p>
+                <select className="select w-full max-w-md border border-green-500 ">
+                    <option disabled selected>location</option>
+                    {category.map((item) => (
+                        <option value={item.category} key={item.image} className="text-slate-800">
+                            {item.location}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </div>
+
     );
 };
 
